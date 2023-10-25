9c73e20c1f58f36301a7166c1ec0a927804f7b62
QuemoZem <fran.tucmascotas@gmail.com>
2023-10-25 20:49:49 -0300

    terminamos con el archivo variables

---
+++ js/variables.js
@@ -0,0 +1,14 @@
+/* Declaraciones de variables y diferncias entre let y const,
+ */
+
+let anioActual = 2023;
+
+/* Enviar a imprimir en Cosola la variable  */
+
+console.log(anioActual);
+console.log("Año Actual : " + anioActual);
+
+/* las variables const
+ */
+
+const numeros = 3.1516;
